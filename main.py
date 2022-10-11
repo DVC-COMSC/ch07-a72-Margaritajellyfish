664fa329c9dfa1a0ad190b0dc73f365ad236e0f4
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 00:28:48 +0000

    7-2

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
-
+count = 0
+for i in range(len(num2)):
+    if num2[i] in num1:
+        count += 1
+if count == len(num2):
+    print(True)
 # ******************************
 # Make your Code
 # ******************************
